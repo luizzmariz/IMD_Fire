b1db760303451606275a19fbccd1c1ce6b32c5ad
CinquilCinquil <106356391+CinquilCinquil@users.noreply.github.com>
2023-11-03 17:55:21 -0300

    limting fire spread

--- Assets/scripts/FireSpread.cs
+++ Assets/scripts/FireSpread.cs
@@ -5,11 +5,13 @@ using UnityEngine;
 public class FireSpread : MonoBehaviour
 {
     public GameObject FireCenter;
+    private Transform IMD_area;
+    public float isolationDisBuffer = 0f;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        IMD_area = GameObject.FindWithTag("IMD_area").gameObject.transform;
     }
 
     // Update is called once per frame
@@ -21,11 +23,16 @@ public class FireSpread : MonoBehaviour
         }
     }
 
+    // Creates a new fire point in 'pos'
     void createNewFireCenter(Vector3 pos)
     {
-        Instantiate(FireCenter, pos, Quaternion.identity);
+        if (isolated(pos) && insideIMD(pos))
+        {
+            Instantiate(FireCenter, pos, Quaternion.identity);
+        }
     }
 
+    // Created 3 new fire points in the shape of a triangle with the center as this object
     public void TriangleSpread(float radius = 12f, float rot = 0f)
     {
         for (int i = 0;i < 3;i ++)
@@ -34,5 +41,30 @@ public class FireSpread : MonoBehaviour
             Vector3 pos = transform.position + radius * (new Vector3(Mathf.Cos(ang), 0, Mathf.Sin(ang)));
             createNewFireCenter(pos);
         }
+    }
+
+    // Returns whether the fire point is far enough from others (aka isolated)
+    private bool isolated(Vector3 pos)
+    {
+        GameObject[] lista = GameObject.FindGameObjectsWithTag("FirePoint");
+
+        foreach(GameObject g in lista)
+        {
+            float dis = Vector3.Distance(g.transform.position, pos);
+            if (dis < g.GetComponent<ParticleSpread>().GetRadius() + isolationDisBuffer)
+            {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    private bool insideIMD(Vector3 pos)
+    {
+        Vector3 topLeft = IMD_area.GetChild(0).transform.position;
+        Vector3 bottomRight = IMD_area.GetChild(1).transform.position;
+
+        return topLeft.x < pos.x && topLeft.z < pos.z && bottomRight.x > pos.x && bottomRight.z > pos.z;
     }
 }
