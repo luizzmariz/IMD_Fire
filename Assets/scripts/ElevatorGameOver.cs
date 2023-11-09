dadf8f4b075b755953288498a8a22c692d52ef8d
CinquilCinquil <106356391+CinquilCinquil@users.noreply.github.com>
2023-11-09 09:14:31 -0300

    fix: elevator GameOver only triggering when there is a fire

--- Assets/scripts/ElevatorGameOver.cs
+++ Assets/scripts/ElevatorGameOver.cs
@@ -13,18 +13,36 @@ public class ElevatorGameOver : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+
     }
 
     private void OnTriggerEnter(Collider other) {
 
         GameObject g = other.gameObject;
 
-        if (g.tag == "Player")
+        if (g.tag == "Player" && (CheckForDanger("FirePoint") || CheckForDanger("Smoke")))
         {
             Player player = g.GetComponent<Player>();
             player.GameOver(player.elevator_death);
         }
 
+    }
+
+    bool CheckForDanger(string danger, float dis_modifier = 1f)
+    {
+        GameObject[] lista = GameObject.FindGameObjectsWithTag(danger);
+
+        foreach(GameObject g in lista)
+        {
+            float distanceToDanger = Vector3.Distance(g.transform.position, transform.position);
+            float dangerRadius = g.GetComponent<ParticleSpread>().GetRadius();
+
+            if (distanceToDanger <= dangerRadius*g.transform.localScale.x*dis_modifier)
+            {
+                return true;
+            }
+        }
+
+        return false;
     }
 }
