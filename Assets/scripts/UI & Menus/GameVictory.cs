865b20f00f151aaf2f0d5894dc3d2ea18eac05c0
CinquilCinquil <106356391+CinquilCinquil@users.noreply.github.com>
2023-11-11 09:54:09 -0300

    added level victory audio

--- Assets/scripts/UI & Menus/GameVictory.cs
+++ Assets/scripts/UI & Menus/GameVictory.cs
@@ -8,11 +8,16 @@ public class GameVictory : MonoBehaviour
 {
 
     public TextMeshProUGUI scoreText;
+    public AudioClip levelcomplete_audio;
+
+    private AudioSource audioSource;
 
     // Start is called before the first frame update
     void Start()
     {
         scoreText.text += PlayerPrefs.GetInt("Score");
+        audioSource = GetComponent<AudioSource>();
+        audioSource.PlayOneShot(levelcomplete_audio);
     }
 
     // Update is called once per frame
